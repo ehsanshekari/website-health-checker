6c3c8553ecc30db460b3ee2f251770d35b6376e9
Ehsan Shekari <e.shekari@gmail.com>
2026-01-17 17:18:08 +0200

    fix: test

--- infra/src/lib/stacks/website-monitoring.stack.ts
+++ infra/src/lib/stacks/website-monitoring.stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { resolve } from 'path';
 import { ApiGatewayConstruct } from '../constructs/api-gateway.construct';
 import { scheduleConfig } from '../../config/schedule.config';
 import { WebsiteCheckerLambda } from '../constructs/website-checker-lambda.construct';
@@ -9,21 +8,20 @@ import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 import { Duration } from 'aws-cdk-lib';
 
-const _handlerBasePath = resolve(__dirname, '../../../service/src');
-
 export class WebsiteMonitoringStack extends cdk.Stack {
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const _region = this.region;
-    const _accountId = this.account;
     const stageName = 'prod';
 
     // Create the website checker lambda
     const websiteChecker = new WebsiteCheckerLambda(this, 'WebsiteChecker', {
       timeout: Duration.seconds(30),
       memorySize: 256,
+      environment: {
+        POWER_TOOLS_SERVICE_NAME: 'WebsiteMonitoring',
+      }
     });
 
     // Create EventBridge rule to trigger the checker on schedule
